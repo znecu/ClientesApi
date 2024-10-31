19da79b145ec5def988f0a89b5eaa8e76b51bdd5
znecu <alma_maria@ucne.edu.do>
2024-10-30 21:42:52 -0400

    Creé el ServicesRegistrar

--- Clientes.Services/DI/ServicesRegistrar.cs
+++ Clientes.Services/DI/ServicesRegistrar.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Clientes.Abstractions;
+using Clientes.Data.DI;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Clientes.Services.DI
+namespace Clientes.Services.DI;
+
+public static class ServicesRegistrar
 {
-    internal class ServicesRegistrar
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
     {
+        services.RegisterDbContextFactory();
+        services.AddScoped<IClientesService, ClientesServices>();
+        return services;
     }
 }
